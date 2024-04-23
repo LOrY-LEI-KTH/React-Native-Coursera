66e5d77a75db832956041b34e01577f63b2bfba3
Yuchen Lei <yuchenl@kth.se>
2024-04-23 16:58:30 +0200

    submit code for welcome screen

--- week1/welcome screen/App.js
+++ week1/welcome screen/App.js
@@ -3,6 +3,8 @@ import { View } from 'react-native';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
+import WelcomeScreen from './components/WelcomeScreen';
+
 export default function App() {
   return (
     <>
@@ -12,8 +14,9 @@ export default function App() {
           backgroundColor: '#495E57',
         }}>
         <LittleLemonHeader />
+        <WelcomeScreen />
       </View>
-      <View style={{ backgroundColor: '#495E57' }}>
+      <View style={{backgroundColor: '#495E57' }}>
         <LittleLemonFooter />
       </View>
     </>
