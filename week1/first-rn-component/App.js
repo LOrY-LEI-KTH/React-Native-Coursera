7c4288c0b6cbde91e68ceeb30d197b13224076e0
Yuchen Lei <yuchenl@kth.se>
2024-04-23 16:25:46 +0200

    update the styling for footer

--- week1/first-rn-component/App.js
+++ week1/first-rn-component/App.js
@@ -3,14 +3,18 @@ import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 export default function App() {
   return (
+    <>
     <View
       style={{
         flex: 1,
         backgroundColor: '#495E57',
-        justifyContent: "space-between"
+        
       }}>
       <LittleLemonHeader/>
+    </View>
+    <View style={{ backgroundColor: '#495E57' }}>
       <LittleLemonFooter/>
     </View>
+    </>
   );
 }
--- week1/first-rn-component/components/LittleLemonFooter.js
+++ week1/first-rn-component/components/LittleLemonFooter.js
@@ -2,15 +2,17 @@ import { View, Text } from 'react-native';
 
 export default function LittleLemonFooter() {
   return (
-    <View style={{ flex: 0.05, backgroundColor: '#F4CE14', alignItems:"center", justifyContent:"center" }}>
+    <View style={{
+         backgroundColor: '#F4CE14',
+         marginBottom: 10,
+         }}>
       <Text
         style={{
-          padding: 10,
-          fontSize: 15,
+          fontSize: 18,
           color: 'black',
           textAlign: 'center',
         }}>
-        All rights reserved by Little Lemon, 2022
+        All rights reserved by Little Lemon, 2022{' '}
       </Text>
     </View>
   );
