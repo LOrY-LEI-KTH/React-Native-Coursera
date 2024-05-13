5f0981d900386dd355496ab113239c7cd041986e
Yuchen Lei <yuchenl@kth.se>
2024-05-13 22:16:35 +0200

    submit code for login screen

--- week2/login page/LoginScreen.js
+++ week2/login page/LoginScreen.js
@@ -1,10 +1,29 @@
-import { ScrollView, Text, StyleSheet} from 'react-native';
+import React, { useState } from 'react'; 
+import { ScrollView, Text, StyleSheet,TextInput} from 'react-native';
 
 export default function LoginScreen() {
+
+  const [email, onChangeEmail] = useState(''); 
+  const [password, onChangePassword] = useState(''); 
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.headerText}>Welcome to Little Lemon</Text>
       <Text style={styles.regularText}>Login to continue </Text>
+     <TextInput 
+      style={styles.input} 
+      value={email} 
+      onChangeText={onChangeEmail} 
+      placeholder={'email'} 
+      keyboardType={'email-address'}
+      /> 
+      <TextInput 
+      style={styles.input} 
+      value={password} 
+      onChangeText={onChangePassword} 
+      placeholder={'password'} 
+      keyboardType={'default'}
+      secureTextEntry={true}
+      /> 
     </ScrollView>
   );
 }
@@ -13,6 +32,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+   input: { 
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: '#EDEFEE',
+    backgroundColor: '#EDEFEE',
+    }, 
   headerText: {
     padding: 40,
     fontSize: 30,
