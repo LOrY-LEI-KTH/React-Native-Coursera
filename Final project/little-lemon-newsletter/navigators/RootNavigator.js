721478fc5ca767b389891dc845dff691a548c5a2
Yuchen Lei <yuchenl@kth.se>
2024-06-27 13:49:30 +0200

    Implement two empty screens in stack navigation

--- Final project/little-lemon-newsletter/navigators/RootNavigator.js
+++ Final project/little-lemon-newsletter/navigators/RootNavigator.js
@@ -7,8 +7,17 @@ const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
   return (
-    <Stack.Navigator>
-      {/* Set up stack navigation to move between welcome screen and subscribe screen here */}
+    <Stack.Navigator initialRouteName="Welcome">
+     <Stack.Screen 
+          name="Welcome" 
+          component={WelcomeScreen} 
+          options={{ title: 'Welcome' }} 
+        />
+        <Stack.Screen 
+          name="Subscribe" 
+          component={SubscribeScreen} 
+          options={{ title: 'Subscribe' }} 
+        />
     </Stack.Navigator>
   );
 };
